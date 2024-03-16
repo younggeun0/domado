491b9d1072087c5aa7a7d3cc24cc3d0f4eb77957
Young Oh <dureng5@gmail.com>
2024-03-16 12:09:48 +0900

    Refactor Inline Component

--- src/renderer/pomodoro/PomodoroTimer.tsx
+++ src/renderer/pomodoro/PomodoroTimer.tsx
@@ -1,28 +1,9 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 // import dayjs from 'dayjs'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 import { v4 as uuidv4 } from 'uuid'
-
-function RefreshSVGIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      // width="16"
-      height="16"
-      fill="currentColor"
-      className="bi bi-arrow-repeat"
-      viewBox="0 0 16 16"
-    >
-      <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" />
-      <path
-        fillRule="evenodd"
-        d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"
-      />
-    </svg>
-  )
-}
 
 const { isDebug } = window.electron
 const TIME_INFO = isDebug
@@ -260,7 +241,20 @@ export default function PomodoroTimer({
         </button>
 
         <button type="button" className="default_btn mx-2" onClick={restart}>
-          <RefreshSVGIcon />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            // width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-arrow-repeat"
+            viewBox="0 0 16 16"
+          >
+            <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" />
+            <path
+              fillRule="evenodd"
+              d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3M3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9z"
+            />
+          </svg>
         </button>
 
         <button type="button" className="default_btn" onClick={() => addMin(-TIME_INFO.ADD_MIN)}>
