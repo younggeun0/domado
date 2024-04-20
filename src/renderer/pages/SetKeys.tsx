c868c5c229791bc7894851893c3e41661e3272e8
Young Oh <dureng5@gmail.com>
2024-04-20 23:55:33 +0900

    그냥쓰기 선택 시 뽀모도로 개수 초기화

--- src/renderer/pages/SetKeys.tsx
+++ src/renderer/pages/SetKeys.tsx
@@ -113,6 +113,10 @@ export default function SetKeys() {
               className="flex w-full justify-center rounded-md bg-neutral-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-700"
               onClick={() => {
                 setUseSync(false)
+                setTodayInfo({
+                  date: todayInfo.date,
+                  count: 0,
+                })
                 navigate('/pomodoro')
               }}
             >
