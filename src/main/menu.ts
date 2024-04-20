afc1511f55f8d1711eddda337d7211200978e702
Young Oh <dureng5@gmail.com>
2024-04-20 14:21:18 +0900

    단축키 메뉴 원복

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -70,6 +70,22 @@ export default class MenuBuilder {
         },
       ],
     }
+    const subMenuEdit: DarwinMenuItemConstructorOptions = {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'Command+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+Command+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'Command+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'Command+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'Command+V', selector: 'paste:' },
+        {
+          label: 'Select All',
+          accelerator: 'Command+A',
+          selector: 'selectAll:',
+        },
+      ],
+    }
     const subMenuViewDev: MenuItemConstructorOptions = {
       label: 'View',
       submenu: [
@@ -140,9 +156,9 @@ export default class MenuBuilder {
     }
 
     if (process.env.NODE_ENV === 'development' || process.env.DEBUG_PROD === 'true') {
-      return [subMenuAbout, subMenuViewDev, subMenuWindow, subMenuHelp]
+      return [subMenuAbout, subMenuEdit, subMenuViewDev, subMenuWindow, subMenuHelp]
     }
-    return [subMenuAbout, subMenuWindow, subMenuHelp]
+    return [subMenuAbout, subMenuEdit, subMenuWindow, subMenuHelp]
   }
 
   buildDefaultTemplate() {
