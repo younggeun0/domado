3c2c91de6ef161960e462fae3c0616ee74428619
Young Oh <dureng5@gmail.com>
2024-06-16 16:09:42 +0900

    three js 3D 모델 추가

---
+++ src/renderer/components/Domado.tsx
@@ -0,0 +1,102 @@
+/* eslint-disable react/no-unknown-property */
+import { useEffect, useRef } from 'react'
+import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber'
+import { OrbitControls, useTexture } from '@react-three/drei'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import tomatoOBJ from 'assets/3dmodel/tomato.obj'
+import tomatoMTL from 'assets/3dmodel/tomato.mtl'
+import tomatoTexture from 'assets/3dmodel/tomato_texture.jpg'
+
+import cupOBJ from 'assets/3dmodel/coffee_cup.obj'
+import cupMTL from 'assets/3dmodel/coffee_cup.mtl'
+import { getTimeInfo } from './pomodoro'
+
+function CoffeeCupModel() {
+  const groupRef = useRef<any>()
+  const cupMaterials = useLoader(MTLLoader, cupMTL)
+  const cupObj = useLoader(OBJLoader, cupOBJ, (loader) => {
+    cupMaterials.preload()
+    loader.setMaterials(cupMaterials)
+  })
+
+  const scale = 15
+  cupObj.scale.set(scale, scale, scale)
+  cupObj.position.set(0, -88, 0)
+
+  return <primitive ref={groupRef} object={cupObj} />
+}
+
+function TomatoModel({ paused }: { paused: boolean }) {
+  const groupRef = useRef<any>()
+  const tomatoMaterials = useLoader(MTLLoader, tomatoMTL)
+  const tomatoObj = useLoader(OBJLoader, tomatoOBJ, (loader) => {
+    tomatoMaterials.preload()
+    loader.setMaterials(tomatoMaterials)
+  })
+
+  const texture = useTexture(tomatoTexture)
+  // 텍스처를 모델에 적용
+  tomatoObj.traverse((child: any) => {
+    if (child.isMesh) {
+      if (child.geometry.attributes.uv) {
+        child.material.map = texture
+      }
+    }
+  })
+
+  tomatoObj.rotation.x = -Math.PI / 2
+
+  // 애니메이션
+  useFrame(() => {
+    if (groupRef.current && !paused) {
+      groupRef.current.rotation.z += 0.01
+    }
+  })
+
+  return <primitive ref={groupRef} object={tomatoObj} />
+}
+
+function CameraSetup({ isRest, remainingTime }: { isRest: boolean; remainingTime: number }) {
+  const { camera } = useThree()
+  const timeInfo = getTimeInfo(window.electron?.isDebug ?? true)
+
+  useEffect(() => {
+    if (isRest) {
+      camera.position.set(25, 20, 0)
+    } else {
+      const codinate = 14 + 36 * (remainingTime / timeInfo[isRest ? 'REST_SEC' : 'POMODORO_SEC'])
+      camera.position.set(codinate, codinate, 0)
+    }
+
+    camera.lookAt(0, 0, 0)
+  }, [camera, isRest, remainingTime, timeInfo])
+
+  return null
+}
+
+export default function Domado({
+  isRest,
+  paused,
+  remainingTime,
+}: {
+  isRest: boolean
+  paused: boolean
+  remainingTime: number
+}) {
+  return (
+    <Canvas
+      camera={{
+        position: [50, 40, 0],
+        fov: 75,
+      }}
+      style={{ position: 'absolute', width: '100vw', height: '100vh', zIndex: -1, background: '#21252B' }}
+    >
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      {isRest ? <CoffeeCupModel /> : <TomatoModel paused={paused} />}
+      <CameraSetup isRest={isRest} remainingTime={remainingTime} />
+      <OrbitControls />
+    </Canvas>
+  )
+}
--- src/renderer/pages/Pomodoro.tsx
+++ src/renderer/pages/Pomodoro.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable consistent-return */
 import { useEffect, useRef, useState } from 'react'
-import { formatRemainingTime, getTimeInfo, updateTray } from '../components/pomodoro'
+import { getTimeInfo, updateTray } from '../components/pomodoro'
 import Footer from '../components/Footer'
+import Domado from '../components/Domado'
 
 export default function Pomodoro() {
   const [todayInfo, setTodayInfo] = useState({ count: 0 })
 
-  // TODO 구현 - restart, 휴식시간 스킵 기능, 시간 조절(필수는 아닐듯)
+  // TODO 구현 - 시간 조절(필수는 아닐듯)
   const [status, setStatus] = useState<'restart' | 'running' | 'finish' | 'paused'>('paused')
   const [isRest, setIsRest] = useState(false)
   const timeInfo = getTimeInfo(window.electron?.isDebug ?? true)
@@ -145,70 +146,40 @@ export default function Pomodoro() {
     }
   }, [])
 
-  const playEmoji = isRest ? '☕️' : '️🔥'
-
   return (
     <div className="relative w-screen h-screen flex flex-col overflow-auto text-gray-600">
+      <Domado isRest={isRest} paused={status === 'paused'} remainingTime={remainingTime} />
+
       <div className="p-3 flex flex-1 flex-col items-center justify-center">
         <div className="w-screen h-full flex flex-col justify-center items-center">
-          <button
-            type="button"
-            className="flex w-full h-100 justify-center rounded-md bg-transparent p-10 text-8xl leading-8 text-white"
-            onClick={togglePlay}
-            tabIndex={-1}
-          >
-            {status === 'paused' ? playEmoji : '⏸️'}
-          </button>
-
-          <div className=" text-sm bottom-4 text-white/70" style={{ zIndex: 10 }}>
-            {formatRemainingTime(remainingTime)}
-          </div>
-
           {/* <div className={`flex justify-center items-center ${!status.endsWith('_start') ? 'invisible' : 'visible'}`}>
-          <button
-            type="button"
-            className="flex w-full justify-center rounded-md bg-neutral-700 px-1 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-700"
-            onClick={() => addMin(TIME_INFO.ADD_MIN)}
-          >
-            +5 min
-          </button>
+            <button
+              type="button"
+              className="flex w-full justify-center rounded-md bg-neutral-700 px-1 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-700"
+              onClick={() => addMin(TIME_INFO.ADD_MIN)}
+            >
+              +5 min
+            </button>
 
-          <button
-            type="button"
-            className="mx-2 h-full flex w-full justify-center rounded-md bg-neutral-700 px-1 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-700"
-            onClick={restart}
-          >
-            RESET
-          </button>
+            <button
+              type="button"
+              className="mx-2 h-full flex w-full justify-center rounded-md bg-neutral-700 px-1 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-700"
+              onClick={restart}
+            >
+              RESET
+            </button>
 
-          <button
-            type="button"
-            className="flex w-full justify-center rounded-md bg-neutral-700 px-1 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-700"
-            onClick={() => addMin(-TIME_INFO.ADD_MIN)}
-          >
-            -5 min
-          </button>
-        </div> */}
+            <button
+              type="button"
+              className="flex w-full justify-center rounded-md bg-neutral-700 px-1 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-neutral-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-700"
+              onClick={() => addMin(-TIME_INFO.ADD_MIN)}
+            >
+              -5 min
+            </button>
+          </div> */}
         </div>
-
-        <div
-          id="bg-timer"
-          className="absolute bottom-0 w-full"
-          style={{
-            zIndex: '-1',
-            background: isRest
-              ? 'linear-gradient(to top, rgb(137, 248, 109), #6bf748, #6bf748, #41e418)'
-              : 'linear-gradient(to bottom, #fa3508, #fa5029, #fa5029, #fa5029, #fa5029)',
-          }}
-        />
-        <div
-          className="absolute bg-gray-800 bottom-0 w-full h-full"
-          style={{
-            zIndex: '-2',
-          }}
-        />
       </div>
-      <Footer todayInfo={todayInfo} />
+      <Footer remainingTime={remainingTime} todayInfo={todayInfo} />
     </div>
   )
 }
